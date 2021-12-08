120ec9a3f8509883ab0a4caf2ac26707cfe6aa68
Bogdan Piele <bogdanpielemail@gmail.com>
2021-12-08 16:21:52 +0200

    Finished part two using deductions based on common letters between signals

--- src/D08/D08.dart
+++ src/D08/D08.dart
@@ -3,50 +3,93 @@ import 'dart:io';
 
 Future<void> main() async {
   List<int> alphabetAscii =
-      new List.generate(26, (index) => 'a'.codeUnitAt(0) + index);
+      new List.generate(7, (index) => 'a'.codeUnitAt(0) + index);
   List<String> alphabet = [];
   alphabet = alphabetAscii
       .map((asciiValue) => String.fromCharCode(asciiValue))
       .toList();
-
-  print(alphabet);
 
   List<String> signalPatterns = [];
   List<String> outputNumbers = [];
 
   await read(signalPatterns, outputNumbers);
 
-  print(countUniques(outputNumbers));
+  signalPatterns = sortStrings(signalPatterns);
+  outputNumbers = sortStrings(outputNumbers);
 
+  print(countUniques(outputNumbers));
   print(findSum(signalPatterns, outputNumbers, alphabet));
 }
 
 int findSum(List<String> signalPatterns, List<String> outputNumbers,
     List<String> alphabet) {
-  List<List<String>> mappinngs = //
-      List.generate(
-          signalPatterns.length,
-          (index) => //
-              List.generate(10, (index) => ""));
+  List<Map<String, int>> mappings = //
+      List.generate(signalPatterns.length, (index) => Map());
+  int sum = 0;
 
   for (int idLine = 0; idLine < signalPatterns.length; ++idLine) {
     String signalPattern = signalPatterns[idLine];
     String outputNumber = outputNumbers[idLine];
     List<String> split = signalPattern.split(" ");
 
+    String one = split.where((signal) => signal.length == 2).first;
+    mappings[idLine][one] = 1;
+
+    String four = split.where((signal) => signal.length == 4).first;
+    mappings[idLine][four] = 4;
+
     String seven = split.where((signal) => signal.length == 3).first;
-    mappinngs[idLine][7] = seven;
+    mappings[idLine][seven] = 7;
+
+    String eight = split.where((signal) => signal.length == 7).first;
+    mappings[idLine][eight] = 8;
 
     String three = //
         split
             .where((signal) =>
                 signal.length == 5 && signal.containsAllOf(seven, alphabet))
             .first;
-    mappinngs[idLine][3] = three;
+    mappings[idLine][three] = 3;
 
-    print(three);
+    String nine = //
+        split
+            .where((signal) =>
+                signal.length == 6 && signal.containsAllOf(three, alphabet))
+            .first;
+    mappings[idLine][nine] = 9;
+
+    String five = //
+        split
+            .where((signal) =>
+                signal.length == 5 &&
+                nine.containsAllOf(signal, alphabet) &&
+                signal.containsAllOf(one, alphabet) == false)
+            .first;
+    mappings[idLine][five] = 5;
+
+    int number = 0;
+    List<String> digits = outputNumber.split(" ");
+
+    for (int idDigit = 0; idDigit < digits.length; ++idDigit) {
+      String digit = digits[idDigit];
+      int? digitValue = 0;
+      if (mappings[idLine].containsKey(digit) &&
+          mappings[idLine][digit] != null) {
+        digitValue = mappings[idLine][digit];
+      } else {
+        if (digit.length == 5) {
+          digitValue = 2;
+        } else if (digit.containsAllOf(five, alphabet))
+          digitValue = 6;
+        else
+          digitValue = 0;
+      }
+      number = number * 10 + digitValue!;
+    }
+
+    sum += number;
   }
-  return 0;
+  return sum;
 }
 
 int countUniques(List<String> outputNumbers) {
@@ -99,4 +142,33 @@ extension StringExtension on String {
     }
     return true;
   }
+}
+
+List<String> sortStrings(List<String> strings) {
+  List<String> sortedStrings = [];
+  strings.forEach(
+    (signal) {
+      String sortedSignal =
+          signal.split(" ").map((string) => sortLetters(string)).join(" ");
+      sortedStrings.add(sortedSignal);
+    },
+  );
+  return sortedStrings;
+}
+
+String sortLetters(String string) {
+  List<int> alphabet =
+      new List.generate(26, (index) => 'a'.codeUnitAt(0) + index);
+
+  List<int> asciiValues = [];
+  alphabet.forEach(
+    (asciiValue) {
+      String letter = String.fromCharCode(asciiValue);
+      if (string.contains(letter)) {
+        asciiValues.add(asciiValue);
+      }
+    },
+  );
+
+  return String.fromCharCodes(asciiValues);
 }
