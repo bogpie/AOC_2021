d20893a2e57c8d397ffb76fc8ebb61f2a6eae156
Bogdan Piele <bogdanpielemail@gmail.com>
2021-12-10 19:20:51 +0200

    Completed D10 part 2 (bracket autocomplete problem) using another stack
    Also removed corrupt lines from part 1

--- src/D10/D10.dart
+++ src/D10/D10.dart
@@ -19,10 +19,10 @@ class D10 {
   };
 
   static Map<String, int> autoCompleteValue = {
-    ')': 1,
-    ']': 2,
-    '}': 3,
-    '>': 4,
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
   };
 
   static Future<void> main() async {
@@ -30,6 +30,7 @@ class D10 {
     await data.read();
 
     print(data.findErrorScore());
+    print(data.findAutoCompleteScore());
   }
 
   Future<void> read() async {
@@ -41,7 +42,9 @@ class D10 {
   int findErrorScore() {
     int score = 0;
     List<String> stack = [];
-    for (String line in lines) {
+
+    for (int idLine = 0; idLine < lines.length; ++idLine) {
+      String line = lines[idLine];
       stack.clear();
       for (int idChar = 0; idChar < line.length; ++idChar) {
         String char = line[idChar];
@@ -57,16 +60,55 @@ class D10 {
             int? value = errorValue[char];
             if (value == null) exit(1);
             score += value;
+            lines[idLine] = '';
             break;
           }
         }
       }
     }
+    lines.removeWhere((line) => line.length == 0);
     return score;
   }
 
   bool isOpen(String char) {
     return char == '(' || char == '[' || char == '{' || char == '<';
+  }
+
+  int findAutoCompleteScore() {
+    List<int> scores = [];
+
+    List<String> stack = [];
+
+    for (final String line in lines) {
+      stack.clear();
+      int score = 0;
+
+      for (int idChar = 0; idChar < line.length; ++idChar) {
+        String char = line[idChar];
+        if (isOpen(char)) {
+          stack.add(char);
+        } else {
+          String top = stack.last;
+          String? topPair = bracketPairs[top];
+          if (topPair == null) exit(1);
+          if (topPair == char) {
+            stack.removeLast();
+          } else {
+            exit(1);
+          }
+        }
+      }
+
+      while (stack.isNotEmpty) {
+        String top = stack.last;
+        int? value = autoCompleteValue[top];
+        score = score * 5 + (value ?? 0);
+        stack.removeLast();
+      }
+      scores.add(score);
+    }
+    scores.sort();
+    return scores[scores.length ~/ 2];
   }
 }
 
